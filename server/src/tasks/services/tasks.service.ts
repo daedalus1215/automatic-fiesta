d6ee2f214611c841415ba0330d50360fa8d355e8
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-05-12 11:51:17 -0400

    add updateDateTimeOfTask

--- server/src/tasks/services/tasks.service.ts
+++ server/src/tasks/services/tasks.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskDocument } from '../schema/task/task.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { UpdateTaskDto } from '../dtos/update-task/update-task.dto';
+import { DateTimeDto } from '../dtos/update-task/date-time.dto';
+import { minutesToMilliseconds } from 'src/utils/minutesToMilliseconds';
 
 @Injectable()
 export class TasksService {
@@ -26,10 +28,29 @@ export class TasksService {
         return await this.model.updateOne({ _id: dto.taskId }, dto);
     }
 
+    async updateDateTimeOfTask(taskId: string, dto: DateTimeDto) {
+        const task = await this.model.findOne({ _id: taskId });
+
+        if (!task) {
+            throw new NotFoundException("task not found");
+        }
+
+        const dateTimes = task.time.map((dateTimeFromDb) => {
+            if (dateTimeFromDb._id == dto._id) {
+                return Object.assign(dateTimeFromDb, { ...dto, time: minutesToMilliseconds(dto.time) });
+            }
+
+            return dateTimeFromDb;
+        });
+
+        task.time = dateTimes;
+        task.date = dto.date;
+        return await task.save();
+    }
+
+    //   updateDateTimeOfTask: (taskId, dateTime) => UpdateDateTimeRepository(taskId, dateTime),
 
     //   fetchAllMonthTasks: (includeTags, excludeTags) => fetchAllMonthTasks(includeTags, excludeTags),
-    //   updateTask: (dto) => updateTask(dto),
-    //   updateDateTimeOfTask: (taskId, dateTime) => UpdateDateTimeRepository(taskId, dateTime),
     //   createDateTimeOfTask: (taskId) => CreateDateTimeRepository(taskId),
     //   fetchAllTaskTitles: (title) => FetchAllTaskTitlesRepository(title),
     //   fetchStackGraph: (date, days, predicates) => FetchStatsForStackForRangeOfDates(date, days, predicates),
