3452c58aed49152baf3b04bb831d3b3ba82af9dc
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-05-30 21:16:25 -0400

    Add test for getTitleType

--- server/src/tasks/services/tasks.service.ts
+++ server/src/tasks/services/tasks.service.ts
@@ -133,62 +133,62 @@ export class TasksService {
     }
 
     // @TODO: Bring this back after we migrate to class utils
-    // async fetchStatsForStackForRangeOfDates(taskId: string) (date, days, predicates) => {
-    //     const dates = getRangeOfDates(date, days);
-    //     const tasks = await getTaskBasedOnTags(predicates);
-    //     let datasets = [];
-    //     for (let task of tasks) {
-    //         const dataset = {
-    //             label: getTitle(task),
-    //             data: Array(days).fill(0),
-    //             backgroundColor: `rgb(${faker.number.int({ min: 100, max: 255 })}, ${faker.number.int({ min: 100, max: 255 })}, ${faker.number.int({ min: 100, max: 255 })})`
-    //         };
+    async fetchStatsForStackForRangeOfDates(date, days, predicates) {
+        const dates = this.dateUtil.getRangeOfDates(date, days);
+        const tasks = await getTaskBasedOnTags(predicates);
+        let datasets = [];
+        for (let task of tasks) {
+            const dataset = {
+                label: getTitle(task),
+                data: Array(days).fill(0),
+                backgroundColor: `rgb(${faker.number.int({ min: 100, max: 255 })}, ${faker.number.int({ min: 100, max: 255 })}, ${faker.number.int({ min: 100, max: 255 })})`
+            };
 
-    //         for (let time of task.time) {
-    //             const currentDate = formatDate(time.date);
-    //             if (currentDate === null || currentDate < dates[dates.length - 1]) {
-    //                 continue;
-    //             }
+            for (let time of task.time) {
+                const currentDate = formatDate(time.date);
+                if (currentDate === null || currentDate < dates[dates.length - 1]) {
+                    continue;
+                }
 
-    //             let index = dates.findIndex(item => {
-    //                 return item === currentDate;
-    //             });
-    //             if (index !== -1) {
-    //                 if (dataset.data[index] >= 0) {
-    //                     dataset.data[index] = dataset.data[index] + (time?.time / 1000 / 60 ?? 0)
-    //                 } else {
-    //                     dataset.data[index] = (time?.time / 1000 / 60 ?? 0)
-    //                 }
-    //             }
-    //         }
+                let index = dates.findIndex(item => {
+                    return item === currentDate;
+                });
+                if (index !== -1) {
+                    if (dataset.data[index] >= 0) {
+                        dataset.data[index] = dataset.data[index] + (time?.time / 1000 / 60 ?? 0)
+                    } else {
+                        dataset.data[index] = (time?.time / 1000 / 60 ?? 0)
+                    }
+                }
+            }
 
-    //         if (dataset.data.find(data => data > 0)) {
-    //             datasets.push(dataset);
-    //         }
-    //     }
+            if (dataset.data.find(data => data > 0)) {
+                datasets.push(dataset);
+            }
+        }
 
-    //     return {
-    //         data: {
-    //             labels: dates,
-    //             datasets
-    //         },
-    //         options: {
-    //             "plugins": {
-    //                 "title": {
-    //                     "display": true,
-    //                     "text": `Stacked Activity over ${days} days`
-    //                 }
-    //             },
-    //             "responsive": true,
-    //             "scales": {
-    //                 "x": {
-    //                     "stacked": true
-    //                 },
-    //                 "y": {
-    //                     "stacked": true
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+        return {
+            data: {
+                labels: dates,
+                datasets
+            },
+            options: {
+                "plugins": {
+                    "title": {
+                        "display": true,
+                        "text": `Stacked Activity over ${days} days`
+                    }
+                },
+                "responsive": true,
+                "scales": {
+                    "x": {
+                        "stacked": true
+                    },
+                    "y": {
+                        "stacked": true
+                    }
+                }
+            }
+        }
+    }
 }
--- server/src/utils/__tests__/string-util.spec.ts
+++ server/src/utils/__tests__/string-util.spec.ts
@@ -1,8 +1,18 @@
+import { Task } from "src/types";
 import { StringUtil } from "../string-util";
+import striptags from "striptags";
+
+jest.mock('striptags', () => ({
+    default: jest.fn().mockImplementation(() => "<p>this will be title</p> Other stuff here that won't get brought into title")
+}));
 
 describe('server/src/utils/__tests__/string-util.spec.ts', () => {
     describe('StringUtil', () => {
         let target = new StringUtil();
+
+        beforeEach(() => {
+            jest.clearAllMocks();
+        });
 
         describe('#truncateString', () => {
             it('should not truncate string when maxLength exceeds string length', () => {
@@ -61,6 +71,66 @@ describe('server/src/utils/__tests__/string-util.spec.ts', () => {
 
                 // Assert
                 expect(actual).toEqual(expected);
+            });
+        });
+
+        describe('#getTitle', () => {
+            it('should return task title when it is on the task', () => {
+                // Arrange
+                const title = 'this will be title';
+
+                const task: Task = {
+                    id: "",
+                    tags: [],
+                    title: title,
+                    description: `<p>not the title!</p> Other stuff here that won't get brought into title`,
+                    date: "",
+                    time: []
+                };
+
+                // Act
+                const actual = target.getTitle(task);
+
+                // Assert
+                expect(striptags).not.toHaveBeenCalled();
+                expect(actual).toEqual(title);
+            });
+
+            it('should return the first paragraph as title, when title does not exist on the task', () => {
+                // Arrange
+                const title = 'this will be title';
+
+                const task: Task = {
+                    id: "",
+                    tags: [],
+                    description: `<p>${title}</p> Other stuff here that won't get brought into title`,
+                    date: "",
+                    time: []
+                };
+
+                // Act
+                const actual = target.getTitle(task);
+
+                // Assert
+                expect(striptags).toHaveBeenNthCalledWith(1, title);
+                expect(actual).toEqual(task.description);
+            });
+
+            it('should return empty string when no title and no description exists on task', () => {
+                // Arrange
+                const task: Task = {
+                    id: "",
+                    tags: [],
+                    date: "",
+                    time: []
+                };
+
+                // Act
+                const actual = target.getTitle(task);
+
+                // Assert
+                expect(striptags).not.toHaveBeenCalled();
+                expect(actual).toEqual('');
             });
         });
     });
