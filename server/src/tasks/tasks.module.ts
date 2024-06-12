62b7525b740e1e2a3d2ec3f35ab1608ef69c9d55
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-06-12 10:43:50 -0400

    abstract fetchAllMonthTasks out of task service.

--- server/src/tasks/tasks.module.ts
+++ server/src/tasks/tasks.module.ts
@@ -4,11 +4,13 @@ import { TasksService } from './services/tasks.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Task, TaskSchema } from './schema/task/task.schema';
 import { UtilModule } from 'src/utils/utils.module';
-import { GroupTitlesBasedOnDateAssembler } from './assemblers/group-titles-based-on-date-assembler';
+import { GroupTitlesBasedOnDateConverter } from './transacription-scripts/fetch-all-month-tasks/converters/group-titles-based-on-date.converter';
+import { GroupTitlesFormatConverter } from './transacription-scripts/fetch-all-month-tasks/converters/group-titles-format.converter';
+import { FetchAllMonthTasks } from './transacription-scripts/fetch-all-month-tasks/fetch-all-month-tasks.transcription-script';
 
 @Module({
   controllers: [TasksController],
-  providers: [TasksService, GroupTitlesBasedOnDateAssembler],
+  providers: [TasksService, GroupTitlesBasedOnDateConverter, GroupTitlesFormatConverter, FetchAllMonthTasks],
   imports: [
     MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
     UtilModule
---
+++ server/src/tasks/transacription-scripts/fetch-all-month-tasks/fetch-all-month-tasks.transcription-script.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { FilterUtil } from "src/utils/filter-util";
+import { GroupTitlesBasedOnDateConverter } from "./converters/group-titles-based-on-date.converter";
+import { InjectModel } from "@nestjs/mongoose";
+import { Task, TaskDocument } from "../../schema/task/task.schema";
+import { Model } from "mongoose";
+import { GroupTitlesFormatConverter } from "./converters/group-titles-format.converter";
+
+//@TODO: Unit test this
+@Injectable()
+export class FetchAllMonthTasks {
+    constructor(@InjectModel(Task.name) private model: Model<TaskDocument>,
+        private readonly filterUtil: FilterUtil,
+        private readonly groupTitlesBasedOnDateConverter: GroupTitlesBasedOnDateConverter,
+        private readonly groupTitlesFormatConverter: GroupTitlesFormatConverter) { }
+
+    async apply(includeTags?: string[], excludeTags?: string[]) {
+        const tasks = await this.model.find();
+        const tagsWithoutExcluded = this.filterUtil.exclusiveFilter(tasks.filter(task => !!task.date), excludeTags);
+        const includedAndExcludedTags = this.filterUtil.inclusiveFilter(tagsWithoutExcluded, includeTags);
+        const results = this.groupTitlesBasedOnDateConverter.apply(includedAndExcludedTags)
+        return this.groupTitlesFormatConverter.apply(results);
+    }
+}
