101486f763405e033571c62b9a3e97f52f42a25d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-07-13 23:39:37 -0400

    quasar: Adding scaffolding, transitioning to task page

--- quasar/src/App.vue
+++ quasar/src/App.vue
@@ -1,17 +1,73 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, provide, onMounted } from 'vue'
+import axios from 'axios'
+import { useRouter } from 'vue-router'
+import SideBar from './components/SideBar.vue'
+
+const drawerLeft = ref(false)
+const router = useRouter()
+
+const tasks = ref([])
+const loading = ref(true)
+const error = ref(null)
+
+const fetchTasks = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/tasks/tasks-titles')
+    tasks.value = response.data
+  } catch (err) {
+    error.value = err.message
+  } finally {
+    loading.value = false
+  }
+}
+
+const forwardToTask = (id) => {
+  console.log('forwardToTask', id)
+  router.push({ name: 'Task', params: { id: id } })
+}
+
+// Call the fetch function when the component is mounted
+onMounted(fetchTasks)
+provide('drawerLeft', drawerLeft)
+provide('forwardToTask', forwardToTask)
+provide('tasks', tasks)
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="q-pa-md">
+    <q-layout
+      view="lhh LpR lFf"
+      container
+      style="height: 500px"
+      class="shadow-2 rounded-borders"
+      :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
+    >
+      <q-header reveal :class="$q.dark.isActive ? 'bg-secondary' : 'bg-black'">
+        <q-toolbar>
+          <q-btn
+            flat
+            @click="drawerLeft = !drawerLeft"
+            round
+            dense
+            icon="menu"
+          />
+          <SideBar />
+          <q-toolbar-title>Header</q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+
+      <q-page-container>
+        <router-view></router-view>
+      </q-page-container>
+
+      <q-footer>
+        <q-toolbar>
+          <q-toolbar-title>Footer</q-toolbar-title>
+        </q-toolbar>
+      </q-footer>
+    </q-layout>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- quasar/src/main.js
+++ quasar/src/main.js
@@ -12,12 +12,14 @@ import 'quasar/src/css/index.sass'
 // Assumes your root component is App.vue
 // and placed in same folder as main.js
 import App from './App.vue'
+import router from './router'
 
 const myApp = createApp(App)
 
 myApp.use(Quasar, {
   plugins: {}, // import Quasar plugins and add here
 })
+myApp.use(router)
 
 // Assumes you have a <div id="app"></div> in your index.html
 myApp.mount('#app')
