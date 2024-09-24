b908bccec728c694b8f16e6cb76e37a18a124715
lala <13687739+daedalus1215@users.noreply.github.com>
2024-09-23 21:30:08 -0400

    tags: Create TagHomePage

---
+++ src/pages/tags/TagHomePage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    import { onMounted, ref, provide } from 'vue';
+    import { fetchTags } from '@utils/fetch';
+    const tags = ref([{}]);
+  
+    onMounted(async () => {
+      const response = await fetchTags();
+      tags.value = response;
+    });
+  
+  </script>
+
+<template>
+     <q-list bordered padding>
+      <q-item v-for="tag in tags" :key="tag._id">
+        <q-item-section>
+          <q-item-label>{{ tag.name }}</q-item-label>
+          <q-item-label caption lines="2">{{ tag.description }}</q-item-label>
+        </q-item-section>
+    </q-item>
+
+    </q-list>
+</template>
